b2598d860be7fb87a0134c5d1cea8d009de6fac3
RyanXi11 <ryanxi57@gmail.com>
2026-05-01 23:56:40 -0400

    Chapter 3: Strings

---
+++ javascript-course/js3/js/main.js
@@ -0,0 +1,32 @@
+//Strings
+const myVariable = "Mathematics";
+
+
+//The length property
+console.log(myVariable.length);
+
+//charAt() method
+console.log(myVariable.charAt(0));
+
+//indexOf() method
+console.log(myVariable.indexOf("at"));
+
+//lastIndexOf() method
+console.log(myVariable.lastIndexOf("at"));
+
+//slice() method
+console.log(myVariable.slice(5,8));
+
+//toUpperCase() method
+console.log(myVariable.toUpperCase());
+
+//toLowerCase() method
+console.log(myVariable.toLowerCase());
+
+//includes() method
+console.log(myVariable.includes("div"));
+console.log(myVariable.includes("mat"));
+
+//split() method
+console.log(myVariable.split("e"));
+console.log(myVariable.split(""));
